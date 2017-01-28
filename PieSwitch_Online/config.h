50fe072bd4a92f0e7e6c30cc676c032f498dbb41
chavee <i@chavee.com>
2017-01-28 20:26:50 +0700

    edit freeboard json

--- PieSwitch_Online/config.h
+++ PieSwitch_Online/config.h
@@ -1,21 +1,10 @@
-//const char* ssid     = "catbus";
-//const char* password = "stud2559";
+#define WIFI_SSID		   "{SSID}"
+#define WIFI_PASSWORD  "{WIFI_PASSWORD}"
 
-//const char* ssid     = "Dookphoo_2.4G";
-//const char* password = "26072527";
+#define APPID          "{APPID}"
+#define KEY            "{KEY}"
+#define SECRET         "{SECRET}"
+#define ALIAS          "{ALIAS}"
 
-//#define WIFI_SSID	   "NSTDA-Project-7013"
-//#define WIFI_PASSWORD  "1q2w3e4r"
-
-#define WIFI_SSID     "MNH811"
-#define WIFI_PASSWORD  "H.MNH811"
-
-
-#define APPID   "iotsw"
-#define KEY     "A1WIcOBToakTmSO"
-#define SECRET  "jDt0f0XWqbBd91w60zoRDzmal"
-#define ALIAS   "pieswitch"
-
-#define FEEDID   "mysensor"
-#define FEEDAPIKEY   "5DunZ38nKP5dGC0h4Bj7mXyeMURdoXOo"
-
+#define FEEDID         "{FEEDID}"
+#define FEEDAPIKEY     "{FEEDAPIKEY}"
